5faf27814309a1e7a0ce2f27744249020473b846
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 23:31:23 +0530

    Vectort class in Collection framework

---
+++ 3- list/1-Vector/VectorApp.java
@@ -0,0 +1,13 @@
+
+
+import java.util.*;
+
+public class VectorApp{
+	public static void main(String x[]){
+		
+		Vector v=new Vector(); // here we use in use of vecotr construtor 
+		
+		int capacity=v.capacity();
+		System.out.println("Capacity of vector is:"+capacity);
+	}
+}
