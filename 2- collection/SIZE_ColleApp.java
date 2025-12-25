68899b895f81ee73ef99e59dd2691225d0ee517a
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-25 14:58:09 +0530

    Collection Interface Method's 1-size()

---
+++ 2- collection/SIZE_ColleApp.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+public class SIZE_ColleApp{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		
+		System.out.println("Size of Collection is:"+al.size());
+	}
+}
