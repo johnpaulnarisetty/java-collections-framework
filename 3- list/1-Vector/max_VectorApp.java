05957d60801550a78c8a5ce948f0fbd48d6df613
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-26 19:00:29 +0530

    In Collection List--> Vector to do diiference operation find max

---
+++ 3- list/1-Vector/max_VectorApp.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class max_VectorApp{
+	public static void main(String x[]){
+		
+		Vector v=new Vector();
+		
+		v.add(10);
+		v.add(20);
+		v.add(70);
+		v.add(40);
+		v.add(50);
+		
+		Iterator i=v.iterator();
+		int max=0;
+		while(i.hasNext()){
+			Object obj=i.next();
+			if((int)obj>max){
+				max=obj;
+			}
+		}
+		
+		System.out.println("Max Element from Vector is: "+max);
+	}
+}
