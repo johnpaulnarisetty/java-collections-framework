8d81b228467c70f88db1141c8186bcb786d60ddf
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-27 11:56:03 +0530

    ArrayListQuestion solution -1

---
+++ 3- list/2-ArrayList/ArrayList_add.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+
+public class ArrayList_add{
+	public static void main(String x[]){
+		
+		ArrayList al=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		System.out.println("ArrayList added element by using add() method: "+al);
+	}
+}
