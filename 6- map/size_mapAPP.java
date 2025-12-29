22bc4f0b254207d2a5aef1a8286eb187feb08aae
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-29 12:10:45 +0530

    Map Interface and it's methods:5-size();

---
+++ 6- map/size_mapAPP.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class size_mapAPP{
+	public static void main(String x[]){
+		
+		HashMap map=new HashMap();
+		map.put(1,"ABC");
+		map.put(2,"PQR");
+		map.put(3,"SUV");
+		map.put(4,"XYZ");
+		
+		int size=map.size();
+		System.out.println("Size of map is:"+size);
+	}
+}	
