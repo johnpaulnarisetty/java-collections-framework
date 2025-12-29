09b56df327c459d8c823ce0ae073281a1615362b
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-29 11:49:16 +0530

    Map Interface and it's methods:1-public abstract V put(k,v);

---
+++ 6- map/put_mapAPP.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+public class put_mapAPP{
+	public static void main(String x[]){
+		
+		HashMap map=new HashMap();
+	
+		map.put(1,10);
+		map.put(2,50);
+		map.put(3,10);
+		map.put(4,20);
+		map.put(5,70);
+		map.put(7,50);
+		
+		System.out.println(map);
+	}
+}
