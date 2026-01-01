57ca9a05f875f43c3322156a995fb29f21edd8c6
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-01 22:53:35 +0530

    Practice on the collections assignments

---
+++ 3- list/2-ArrayList/MergeArrayListApp.java
@@ -0,0 +1,51 @@
+
+/*
+Q2. Create two ArrayLists containing integers. Merge them into a new ArrayList but avoid adding repeated numbers.
+What you practice:
+Merging lists
+Checking for duplicates (contains)
+Building logical flow to avoid sets
+*/
+
+import java.util.*;
+public class MergeArrayListApp{
+	public static void main(String x[]){
+		ArrayList al=new ArrayList();
+		ArrayList al2=new ArrayList();
+		ArrayList al3=new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		al2.add(20);
+		al2.add(60);
+		al2.add(10);
+		al2.add(70);
+		al2.add(80);
+		
+		Iterator i=al.iterator();
+		while(i.hasNext()){
+			Object obj=i.next();
+			if(al.contains(obj)){
+				al3.add(obj);
+			}
+		}
+		
+		Iterator j=al2.iterator();
+		while(i.hasNext()){
+			Object obj=j.next();
+			if(al.contains(obj)){
+				al3.add(obj);
+			}
+		}
+		
+		Iterator k=al3.iterator();
+		while(i.hasNext()){
+			Object obj=k.next();
+			System.out.println(obj);
+		}
+	}
+}
