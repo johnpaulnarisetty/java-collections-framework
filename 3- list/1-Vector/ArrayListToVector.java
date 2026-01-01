2668d93157dc23af97e01608354c3a70171a27a3
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-01 23:07:53 +0530

    Practice on the collections assignments

---
+++ 3- list/1-Vector/ArrayListToVector.java
@@ -0,0 +1,37 @@
+/*
+Q3. Take an ArrayList of integers, convert it to a Vector, then sort the Vector in ascending order manually (without using Collections.sort).
+What you practice:
+ArrayList → Vector conversion
+Manual sorting (bubble sort / selection sort)
+Vector methods like add(), get(), set()
+*/
+
+import java.util.*;
+public class ArrayListToVector{
+	public static void main(String x[]){
+		ArrayList al=new ArrayList();
+		
+		al.add(50);
+		al.add(10);
+		al.add(30);
+		al.add(10);
+		al.add(20);
+		al.add(40);
+		
+		Vector v=new Vector(al);
+		
+		for(int i=0; i<v.size(); i++){
+			for(int j=i+1; j<v.size(); j++){
+				Object obj1=i.next();
+				Object obj2=j.next();
+				if((int)obj1>(int)obj2){
+					int temp=obj1;
+					obj1=obj2;
+					obj2=temp;
+				}
+			}
+		}
+		
+		System.out.println(v);
+	}
+}
